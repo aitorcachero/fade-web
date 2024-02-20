6d838e4572da3f57e9811e81f8a4d7502858b09f
Aitor Cachero <aitor.cachero@gmail.com>
2024-02-20 11:29:22 +0100

    Fix

---
+++ .history/src/Pages/Classes/Sacerdote/Sacerdote_20240220112855.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import sacerImg from '../../../assets/clases/priest.png';
+import PriestDisci from './PriestDisci';
+import PriestHoly from './PriestHoly';
+import PriestShadow from './PriestShadow';
+import checkImageType from '../../../helpers/checkImageType';
+import { buttonSpec } from '../../../helpers/styles';
+
+export default function Monje({ data }) {
+  const [spec, setSpec] = useState();
+
+  return (
+    <div className="relative w-full  flex flex-col justify-center items-center  p-20">
+      <article className="border border-orange-500 rounded-lg w-full relative p-10 flex flex-row justify-between items-center shadow-md shadow-orange-800 h-56 mb-24 bg-slate-950">
+        <h1>
+          Los sacerdotes usan poderosa magia de sanación para asegurarse de que
+          tanto ellos como sus compañeros no son derribados. También controlan
+          poderosos hechizos ofensivos a distancia, pero pueden verse
+          sobrepasados por los enemigos debido a su fragilidad física y a su
+          ligera armadura. Los sacerdotes más experimentados combinan el uso de
+          sus hechizos ofensivos y de control con la importancia de mantener
+          vivo al resto del grupo.
+        </h1>
+        <img
+          src={sacerImg}
+          className="z-10"
+          style={{ width: 600, height: 456 }}
+        />
+      </article>
+      <div className="flex flex-row gap-5 mb-10">
+        {data.specs.map((spec, i) => {
+          return (
+            <button
+              className={buttonSpec}
+              key={i}
+              onClick={() => {
+                setSpec(spec.priority);
+              }}
+            >
+              {spec.nombre}
+              <img src={checkImageType(spec.type)} width={40} />
+            </button>
+          );
+        })}
+      </div>
+      {spec && spec === 'disci' && <PriestDisci />}
+      {spec && spec === 'priestholy' && <PriestHoly />}
+      {spec && spec === 'shadow' && <PriestShadow />}
+    </div>
+  );
+}
