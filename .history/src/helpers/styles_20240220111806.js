6d38a28767d345886287bb5384c433d470836073
Aitor Cachero <aitor.cachero@gmail.com>
2024-02-20 11:27:41 +0100

    Last changes

---
+++ .history/src/helpers/styles_20240220111806.js
@@ -0,0 +1,4 @@
+const buttonSpec =
+  'inline-flex  min-w-40 animate-background-shine items-center justify-center rounded-md border border-gray-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors z-50 gap-2 p-4';
+
+export { buttonSpec };
