b78ba3979a7180d0128f3c8bebc4c2daebcff933
Aitor Cachero <aitor.cachero@gmail.com>
2024-02-20 18:42:37 +0100

    Add druid class container

---
+++ .history/src/Pages/Classes/Druida/DruidaHeal_20240209154213.jsx
@@ -0,0 +1,9 @@
+export default function DruidaHeal() {
+  return (
+    <div className="w-full flex flex-col justify-center items-center gap-2  mb-10">
+      <span className="bg-gradient-to-t from-[#c7d2fe] to-[#8678f9] bg-clip-text text-xl text-transparent">
+        {`Intelecto > Espíritu (hasta que la regeneración de maná sea optima) > Celeridad (hasta conseguir los caps) > Maestría > Crítico`}
+      </span>
+    </div>
+  );
+}
