37ec2476a90708e66470031b4ee01c6733c1e4dc
Aitor Cachero <aitor.cachero@gmail.com>
2024-02-20 17:27:04 +0100

    Add BG HomPage

---
+++ .history/src/Pages/HomePage/HomePage_20240220172315.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <article className="p-10 mt-96">
+        <h1 className="text-7xl mb-5">
+          Bienvenidos a <span className="text-7xl text-orange-500">FADE</span>
+        </h1>
+        <p>Guild Alianza en Stormforge</p>
+      </article>
+    </div>
+  );
+}
---
+++ .history/src/Pages/HomePage/HomePage_20240220172354.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col w-full justify-center items-center bg-slate-800 "
+            style={{
+              backgroundImage: `url(${pandariaIMG})`,
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundAttachment: 'fixed',
+            }}
+      <article className="p-10 mt-96">
+        <h1 className="text-7xl mb-5">
+          Bienvenidos a <span className="text-7xl text-orange-500">FADE</span>
+        </h1>
+        <p>Guild Alianza en Stormforge</p>
+      </article>
+    </div>
+  );
+}
+
+
+
